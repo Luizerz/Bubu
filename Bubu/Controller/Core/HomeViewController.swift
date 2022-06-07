1225a167c5b5d65e9069a21ae0abe9a4728f315a
João Victor Ipirajá de Alencar <joaovictorpiraja@gmail.com>
2022-06-06 21:31:00 -0300

    Add Constraints

--- Bubu/Controller/Core/HomeViewController.swift
+++ Bubu/Controller/Core/HomeViewController.swift
@@ -8,25 +8,28 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    
+
     private let label: UILabel = {
         let label = UILabel()
         label.text = "Teste"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .right
+        label.tintColor = .blue
         return label
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         view.addSubview(label)
-        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            label.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        ])
     }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        label.frame = CGRect(x: 20, y: view.height - view.safeAreaInsets.bottom - 50, width: view.width-40, height: 50)
-    }
-
-
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+////        label.frame =
+////        CGRect(x: 20, y: view.height - view.safeAreaInsets.bottom - 50, width: view.width-40, height: 50)
+//    }
 }
-
