aab0d1ea253d2f9ebb903237abcf509ae9049c51
João Victor Ipirajá de Alencar <joaovictorpiraja@gmail.com>
2022-06-13 11:37:39 -0300

    Change Root View Controller

--- Bubu/Resources/AppDelegate.swift
+++ Bubu/Resources/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = HomeViewController()
+        window.rootViewController = ChallengeViewController()
         window.makeKeyAndVisible()
         self.window = window
         return true
--- Bubu/Resources/SceneDelegate.swift
+++ Bubu/Resources/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = HomeViewController()
+        window.rootViewController =  ChallengeViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
