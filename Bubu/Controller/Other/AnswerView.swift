5fde912109088433929ed0d4ae74f4355e0e011e
João Victor Ipirajá de Alencar <joaovictorpiraja@gmail.com>
2022-06-18 11:30:43 -0300

    Update AnswerView.swift

--- Bubu/Controller/Other/AnswerView.swift
+++ Bubu/Controller/Other/AnswerView.swift
@@ -7,70 +7,59 @@
 
 import UIKit
 
-struct Answer{
-    var content:Number
-    var isTrue:Bool = false
-}
-
 class AnswerView: UIView {
-    
-    
-    
+
     private var model: [Answer] = []
-    
     lazy var listButton: [UIButton] = self.model.map { answer in
         let button = UIButton(frame: .zero)
-        
-        button.setImage(answer.content.image, for: .normal)
-        
+
+        button.setImage(answer.contentImage, for: .normal)
+
         let action = UIAction { _ in
             print(answer.isTrue)
         }
-        
+
         button.addAction(action, for: .primaryActionTriggered)
         button.layoutIfNeeded()
         button.subviews.first?.contentMode = .scaleAspectFit
         return button
     }
-    
+
     lazy var stackView = UIStackView(arrangedSubviews: listButton)
 
-    
     convenience init(answerModel: [Answer]? = nil) {
         self.init(frame: .zero)
-        
+
         self.model = answerModel ?? []
-        
-        //backgroundColor = .systemGreen
-        
+
+        // backgroundColor = .systemGreen
+
         self.addSubview(stackView)
-        
+
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         NSLayoutConstraint.activate([
             stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
             stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
             stackView.topAnchor.constraint(equalTo: topAnchor),
-            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
-        
+
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
         stackView.spacing = UIStackView.spacingUseSystem
-        
+
         self.stackView.isLayoutMarginsRelativeArrangement = true
 
         self.stackView.layoutMargins = UIEdgeInsets(top: 30, left: 20, bottom: 30, right: 20)
         self.translatesAutoresizingMaskIntoConstraints = false
 
-        
     }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-    
-    
+
 //    init(){
 //        super.init(arrangedSubviews: )
 //        self.axis = .horizontal
@@ -80,13 +69,9 @@ class AnswerView: UIView {
 //
 
 //    }
-    
+
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
-    
-    
 
 }
