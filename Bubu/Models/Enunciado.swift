489ea174a39579cc1893bbe2f7a5c78414b24d06
João Victor Ipirajá de Alencar <joaovictorpiraja@gmail.com>
2022-06-21 09:23:57 -0300

    Add array of Answer in Enunciado Struct

--- Bubu/Models/Enunciado.swift
+++ Bubu/Models/Enunciado.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 struct Enunciado {
+    
     var numberLeftImage: UIImage {
         get {
             return self.numberLeft.image(level: self.level)
@@ -25,32 +26,28 @@ struct Enunciado {
             return self.numberRight.image(level: self.level)
         }
     }
+    
+    var answers:[Answer]
 
-    var answers: [Answer] {
-        get {
-            return self.answerArray.map({
-                Answer(content: $0.content, level: self.level, isTrue: $0.isTrue)
-            })
-        }
-
-    }
     
   
     private var operation: Operation
     private var numberLeft: Number
     private var numberRight: Number
-    private var level: Level = .nolevel
-    private var answerArray: [Answer]
+    private var level: Level
+    
 
     init(numberLeft: Number, operation: Operation, numberRight: Number, answers: [Answer]) {
         self.numberLeft = numberLeft
         self.operation = operation
         self.numberRight = numberLeft
-        self.answerArray = answers
+        self.answers = answers
+        self.level = .nolevel
     }
 
     init(level: Level, enunciado: Enunciado) {
         self = enunciado
+        print(self.numberLeft.rawValue)
         self.level = level
     }
 
