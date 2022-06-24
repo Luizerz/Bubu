972b9b5581a9aae91fc40f25abe9dedeca8ac3db
João Victor Ipirajá de Alencar <joaovictorpiraja@gmail.com>
2022-06-23 23:24:41 -0300

    Update Navigation

--- Bubu/Controller/Core/ChallengeViewController.swift
+++ Bubu/Controller/Core/ChallengeViewController.swift
@@ -75,15 +75,7 @@ class ChallengePageViewController: UIPageViewController, UIPageViewControllerDat
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let navBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 44))
-        view.addSubview(navBar)
-
-        let navItem = UINavigationItem(title: "SomeTitle")
-        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: nil)
-        navItem.rightBarButtonItem = doneItem
-
-        navBar.setItems([navItem], animated: false)
+        self.navigationItem.title = "Trdte"
 
         self.delegate = self
         self.dataSource = self
